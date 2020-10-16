cd9d9572aed3040e4629b1b08426ec4a61bf87ac
Feda Zidan <fido311@gmail.com>
2020-10-15 23:59:53 -0500

    Fished setting up the files

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("dotenv").config();
+
+// mydql connection information
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    //Username and password
+    user: "root",
+    password: process.env.MYSQLPW,
+    database: "employee_trackerDB"
+});
+
+// to test connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured)
+VALUES
+("Cheeseburger", false),
+("Swiss Mushroon Burger", false),
+("Protein-Powered Veggie Burger", false);
