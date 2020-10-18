48cd4f6bf9166d536892401aaa9003c328671684
Feda Zidan <fido311@gmail.com>
2020-10-18 13:27:22 -0500

    Adding the code to hook up the app to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,20 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("dotenv").config();
+//Connecting to JAWSDB
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        //Username and password
+        user: "root",
+        password: process.env.MYSQLPW,
+        database: "burgers_db"
+    });
+};
 
-// mydql connection information
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    //Username and password
-    user: "root",
-    password: process.env.MYSQLPW,
-    database: "burgers_db"
-});
-
-// to test connection
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
